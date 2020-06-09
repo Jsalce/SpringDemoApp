10e37031952cfe558c737abc8351aca6d4a46f4c
PC <PC@DESKTOP-P2KUKHU>
2020-06-08 21:54:06 -0500

    First commit of my work / 25/05

---
+++ SpringDemoApp/src/main/java/idat/edu/pe/demo/SpringDemoAppApplication.java
@@ -0,0 +1,14 @@
+package idat.edu.pe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAppApplication.class, args);
+		System.out.println("Mi primer spring en STS");
+	}
+
+}
---
+++ SpringDemoApp/src/test/java/idat/edu/pe/demo/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
